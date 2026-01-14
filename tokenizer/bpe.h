5bca85c26658f374ce5b05e70b210f79f60f258c
Marvin-desmond <marvindesmond@gmail.com>
2026-01-14 09:09:02 -0500

    add error macro, include load and hash utils, add param to BPE func

--- tokenizer/bpe.h
+++ tokenizer/bpe.h
@@ -1,7 +1,20 @@
 #ifndef BPE_H
 #define BPE_H
-#include <stdio.h>
+#define CHECK_ERR(ptr, err) do { \
+    if (!(ptr)) { \
+    printf("%s\n", err); \
+    exit(EXIT_FAILURE); \
+    } \
+} while(0)
 
-void BPE();
+#include <stdio.h>
+#include <stdlib.h>
+#include "uthash.h"
+#include "cJSON.h"
+
+size_t get_file_size(FILE *f);
+static char *read_file(char *fname);
+
+void BPE(char *tok_path);
 
 #endif
