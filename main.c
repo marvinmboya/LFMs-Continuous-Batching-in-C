f6a1fb027d6be7b32f98fc8b12ef1f6c401cd65c
Marvin-desmond <marvindesmond@gmail.com>
2026-01-16 00:17:49 -0500

    test encoding, decoding using loaded tokenizer

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include "lfm.h" 
-#include "bpe.h"
 // also can use <lfm.h>
+#include "bpe.h"
 
 int main() {
-    char *tok_path = "files/tokenizer.json";
-    BPE(tok_path);
-    LFM();
-    printf("Hello Main Program.\n");
+    char *tok_path = "files/tokenizer.bin";
+    Tokenizer *tok = init_tok_special_toks(tok_path);
+
+    const char *text = "<|startoftext|>Hello world! And again!\n<|im_start|>What's the new image?<|im_end|>assistant";
+    int len;
+    int *tokens = encode(tok, text, &len);
+    for (int i = 0; i < len; i++) printf("%d ", tokens[i]);
+    printf("\n");
+    char *decoded = decode(tok, tokens, len);
+    printf("%s\n", decoded);    
+    free(decoded); free(tokens);
+    return 0;
 }
