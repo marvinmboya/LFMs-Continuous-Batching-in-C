d9fed16c500a1192d5c1253dbae24f716ae72380
Marvin-desmond <marvindesmond@gmail.com>
2026-01-17 07:18:07 -0500

    use lfm n_vocab, d_model values, print first values for check

--- bin-read-write/bin_write.py
+++ bin-read-write/bin_write.py
@@ -2,14 +2,15 @@ import torch, torch.nn as nn
 from torch import bfloat16, int16
 
 torch.manual_seed(42)
-n_vocab, d_model = 2, 3
-a = nn.Embedding(n_vocab, d_model, dtype=bfloat16)
+n_vocab, d_model = 65_536, 1_024 
+embed = nn.Embedding(n_vocab, d_model, dtype=bfloat16)
 
 in_ = torch.randint(0, n_vocab, (1, 5))
-print(a(in_))
-a_flat = a.weight.data.clone().flatten()
-data = a_flat.cpu().view(dtype=int16)
-print(a_flat)
+print(in_)
+print(embed(in_)[0, :, :5])
+embed_flat = embed.weight.data.clone().flatten()
+data = embed_flat.cpu().view(dtype=int16)
+# print(a_flat)
 data.numpy().tofile('data.bin')
 print(f"Saved {len(data)} bfloat16 values to 'data.bin'")
 
