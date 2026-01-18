6920b3aac2eb51b9b3c313c2831894200b4ed773
Marvin-desmond <marvindesmond@gmail.com>
2026-01-18 07:47:57 -0500

    function prototypes for reading bf16 data

--- model/utils.h
+++ model/utils.h
@@ -2,6 +2,7 @@
 #define ERR_H
 #include <stdint.h>
 #include <stdio.h>
+#include <sys/stat.h> // fstat(...)
 #include <omp.h>
 
 #define PERR(err) do { \
@@ -10,6 +11,9 @@
 } while(0)
 
 float bf16_to_float32(uint16_t b);
+int get_suff_threads();
 void get_threads_info();
-
+size_t get_file_size(FILE *f);
+size_t get_file_size_v2(FILE *f);
+uint16_t *read_bin_data(const char *data_path, size_t n);
 #endif
