b07311f89282df8ce661a353303dc963f13871e1
Marvin-desmond <marvindesmond@gmail.com>
2026-01-19 03:19:29 -0500

    function prototypes for var, rsqrt, and norm functions

--- model/rmsnorm.h
+++ model/rmsnorm.h
@@ -1,4 +1,12 @@
 #ifndef RMS_H
 #define RMS_H
+#include <string.h>
+#include "utils.h"
+#include <math.h>
 
+void compute_rms_norm(float *data, float *weights, size_t n, int d_model);
+
+void compute_vars(float *data, float *vars, size_t n, int d_model);
+void compute_rsqrt(float *data, float *vars, int n_vars, size_t n, int d_model);
+void compute_norm_out(float *data, float *weights, size_t n, int d_model);
 #endif
