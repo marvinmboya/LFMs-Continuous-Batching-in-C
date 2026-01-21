a5185077da10c8219be166c67302466bf05a02e7
Marvin-desmond <marvindesmond@gmail.com>
2026-01-21 04:23:01 -0500

    fix correct transpose dims

--- model/attention/utils.c
+++ model/attention/utils.c
@@ -1,13 +1,13 @@
 #include "utils.h"
 
 void transpose_middle(
-    int BATCH, int heads, int seq_len, int head_dim, 
+    int BATCH, int seq_len, int heads, int head_dim, 
     float *old, float *new) {
-    int C = heads, H = seq_len, W = head_dim;
+    int C = seq_len, H = heads, W = head_dim;
     #pragma omp parallel for collapse(2)
     for (int n = 0; n < BATCH; n++) {
         for (int h = 0; h < H; h++) {
-            for (int c = 0; c < heads; c++) {
+            for (int c = 0; c < C; c++) {
                 int dst_base = n*(H*C*W) + h*(C*W) + c*W;
                 int src_base = n*(C*H*W) + c*(H*W) + h*W;
                 #pragma omp simd
