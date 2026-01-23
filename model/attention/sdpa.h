30f2597170cfae692dfe06b0de99d23f71095251
Marvin-desmond <marvindesmond@gmail.com>
2026-01-23 06:52:18 -0500

    implement sdpattention and the function prototypes involved

---
+++ model/attention/sdpa.h
@@ -0,0 +1,13 @@
+#ifndef SDPA_H
+#define SDPA_H
+#include <stdlib.h> // size_t
+#include "linear.h"
+#include "utils.h"
+
+float *sdpattention(
+    float *q, float *k, float *v, float scale,
+    int BATCH, int seq_len, int heads, int head_dim
+);
+void softmax_last(
+    const float *in, float *out, int B, int H, int S, int HD);
+#endif
