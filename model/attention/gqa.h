aa0dfa6b888833f6f961d159643114a2f875741f
Marvin-desmond <marvindesmond@gmail.com>
2026-01-23 06:52:53 -0500

    import sdpa

--- model/attention/gqa.h
+++ model/attention/gqa.h
@@ -1,9 +1,10 @@
 #ifndef GQA_H
 #define GQA_H
 #include "linear.h"
-#include "utils.h"
-#include "../types.h"
+#include "sdpa.h"
 #include "../rmsnorm.h"
+#include "../types.h"
+#include "utils.h"
 
 void gqattention(
     float *x_in, LFM2Config *config, float *qkv_weights, 
