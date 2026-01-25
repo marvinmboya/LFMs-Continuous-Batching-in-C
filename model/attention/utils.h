d03993dfa52537d96a1518b735d944a7fae7a08b
Marvin-desmond <marvindesmond@gmail.com>
2026-01-25 12:18:39 -0500

    function prototype for transposing last two dimes for 3D tensor

--- model/attention/utils.h
+++ model/attention/utils.h
@@ -6,9 +6,14 @@ void transpose_middle(
     int BATCH, int heads, int seq_len, int head_dim, 
     const float *old, float *new);
 
-void transpose_last(
+void transpose_last_higher(
     int BATCH, int seq_len, int heads, int head_dim,
     const float *old, float *new);
+
+void transpose_last(
+    int BATCH, int seq_len, int dims,
+const float *old, float *new);
+
 void repeat_interleave(
     const float *in, size_t n, 
     float *out, int block_size, int repeats
