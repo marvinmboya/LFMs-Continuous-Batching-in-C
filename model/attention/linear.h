a39d9047c509daa749de8a8149a8ad0662bf990b
Marvin-desmond <marvindesmond@gmail.com>
2026-01-25 03:22:05 -0500

    matmul funcs inside sdpa

--- model/attention/linear.h
+++ model/attention/linear.h
@@ -4,4 +4,8 @@
 
 void matmul(
     const float *x_in, const float *weights, float *x_out, int BATCH, int M, int K, int N);
+void matmul_higher(
+    const float *A, const float *B, float *C, int BATCH, int heads, int seq_len, int head_dim);
+void matmul_implicit_transpose(
+    const float *Q, const float *K, float *QKT, int BATCH, int heads, int seq_len, int head_dim);
 #endif
