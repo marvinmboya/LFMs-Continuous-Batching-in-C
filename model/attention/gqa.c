cf81baf4117ddb7295c2d2dd34c6f1aa9d33d72a
Marvin-desmond <marvindesmond@gmail.com>
2026-01-25 05:00:53 -0500

    compute gqa out

--- model/attention/gqa.c
+++ model/attention/gqa.c
@@ -3,7 +3,7 @@
 static float *compute_qkv_outs(float *x_in, const float *assoc_weights, int BATCH, int seq_len, int d_model, int d_out);
 
 void gqattention(
-    float *x_in, LFM2Config *config, float *qkv_weights, 
+    float *x_in, LFM2Config *config, float *qkv_weights, float *wo_weight,
     float *q_norm, float *k_norm, int BATCH, int seq_len
 ) {
     int d_model = config->d_model,
@@ -22,14 +22,14 @@ void gqattention(
     float *q = compute_qkv_outs(x_in, q_weights, BATCH, seq_len, d_model, d_out);
     float *k = compute_qkv_outs(x_in, k_weights, BATCH, seq_len, d_model, kv_d_out);
     float *v = compute_qkv_outs(x_in, v_weights, BATCH, seq_len, d_model, kv_d_out);
-    float *q_trans = malloc(seq_len * d_out * sizeof(float));
-    float *k_trans = malloc(seq_len * kv_d_out * sizeof(float));
-    float *v_trans = malloc(seq_len * kv_d_out * sizeof(float));
-    transpose_middle(BATCH, seq_len, heads, head_dim, q, q_trans);
-    transpose_middle(BATCH, seq_len, kv_groups, head_dim, k, k_trans);
-    transpose_middle(BATCH, seq_len, kv_groups, head_dim, v, v_trans);
+    float *q_t = malloc(seq_len * d_out * sizeof(float));
+    float *k_t = malloc(seq_len * kv_d_out * sizeof(float));
+    float *v_t = malloc(seq_len * kv_d_out * sizeof(float));
+    transpose_middle(BATCH, seq_len, heads, head_dim, q, q_t);
+    transpose_middle(BATCH, seq_len, kv_groups, head_dim, k, k_t);
+    transpose_middle(BATCH, seq_len, kv_groups, head_dim, v, v_t);
     free(q); free(k); free(v);
-    q = q_trans; k = k_trans; v = v_trans;
+    q = q_t; k = k_t; v = v_t;
     compute_rms_norm(q, q_norm, seq_len * d_out, head_dim);
     compute_rms_norm(k, k_norm, seq_len * kv_d_out, head_dim);
     int kv_size = BATCH * seq_len * kv_groups * head_dim;
@@ -41,7 +41,12 @@ void gqattention(
     float *out = sdpattention(
     q, k_expand, v_expand, head_dim,
     BATCH, seq_len, heads, head_dim);
+    float *out_t = malloc(kv_size * group_size * sizeof(float));
+    transpose_middle(BATCH, heads, seq_len, head_dim, out, out_t);
     free(out);
+    float *gqa_out = compute_qkv_outs(out_t, wo_weight, BATCH, seq_len, d_model, d_out);
+    free(out_t);
+    free(gqa_out);
     free(k_expand); free(v_expand);
     free(q);
 }
