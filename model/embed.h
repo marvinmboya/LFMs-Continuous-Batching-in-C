c114756dcaec903efaab0c1489371b39396543f3
Marvin-desmond <marvindesmond@gmail.com>
2026-01-27 06:11:21 -0500

    remove funcs replaced by load and free mmaped weights

--- model/embed.h
+++ model/embed.h
@@ -7,11 +7,8 @@
 #include <sys/stat.h> // fstat(...)
 #include <sys/mman.h> // mmap(...)
 #include <omp.h>
-
 #include "types.h"
 #include "utils.h"
 
-uint16_t* init_embed(const char *embed_path, LFM2Config *config);
-float* compute_embed(uint16_t *embed_data, int *seq, int seq_len, int d_model);
-void free_embed();
+float* compute_embed(float *embed_data, int *seq, int seq_len, int d_model);
 #endif
