3eac8f7d4752eaab05559bdf2563d2fdade6d860
Marvin-desmond <marvindesmond@gmail.com>
2026-01-27 10:59:25 -0500

    move loop to parent function calling backbone

--- model/backbone/backbone.c
+++ model/backbone/backbone.c
@@ -2,29 +2,18 @@
 static const int is_attn_check[16] = {
     0,0,1,0,0,1,0,0,1,0,1,0,1,0,1,0
 };
-void swap(float *p1, float *p2);
 
 void backbone(
-    float *x, float *x_out, LFM2Config *config,
-    GQAWeights *gqa_w, GSCWeights *gsc_w,
-    int batch, int seq_len, int d_model, int k_size
+    float *x, float *x_out, LFM2Config *config, Weights *weights,
+    int batch, int seq_len, int d_model, int k_size,
+    int layer_index
 ){
-    for (int i = 0; i < 16; i++) {
-        compute_rms_norm(x, gqa_w->q_norm, // CHANGE THIS
-            batch * seq_len * d_model, d_model);
-        if (is_attn_check[i]) {
-            gqattention(x, x_out, config, gqa_w, batch, seq_len);
-        } else {
-            gscblock(x, x_out, gsc_w, batch, seq_len, d_model, k_size);
-        }
-        swap(x, x_out);
+    compute_rms_norm(x, weights->rms_before[layer_index],
+        batch * seq_len * d_model, d_model);
+    if (is_attn_check[layer_index]) {
+        gqattention(x, x_out, config, &weights->gqa[layer_index], batch, seq_len);
+    } else {
+        gscblock(x, x_out, &weights->gsc[layer_index], batch, seq_len, d_model, k_size);
     }
-
-}
-
-void swap(float *p1, float *p2) {
-    int temp;
-    temp = *p1;
-    *p1 = *p2;
-    *p2 = temp;
+    x = x_out;
 }
