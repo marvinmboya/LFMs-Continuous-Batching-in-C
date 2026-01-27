d315eaa4c9b52b2d0aaf71003420c733d514bc38
Marvin-desmond <marvindesmond@gmail.com>
2026-01-27 11:04:17 -0500

    implement model func

--- model/lfm.c
+++ model/lfm.c
@@ -1,5 +1,19 @@
 #include "lfm.h"
 
-void LFM() {
+void LFM2Model(
+    int *token_ids, int seq_len, 
+    LFM2Config *config, Weights *weights, int batch
+) {
+    float *embeds_out = malloc(batch * seq_len * config->d_model * sizeof(float));
+    compute_embeds(weights->embeds, embeds_out, token_ids, seq_len, config->d_model);
+    float *x_out = malloc(batch * seq_len * config->d_model * sizeof(float));
+    for (int i = 0; i < 16; i++) {
+        backbone(   
+            embeds_out, x_out, config, weights, 
+            batch, seq_len, config->d_model, config->k_size, i
+        );
+    }
     printf("Hello LFM Model.\n");
+    free(x_out);
+    free(embeds_out);
 }
--- model/lfm.h
+++ model/lfm.h
@@ -1,7 +1,9 @@
 #ifndef LFM_H
 #define LFM_H
 #include <stdio.h>
+#include "types.h"
+#include "embed.h"
+#include "backbone/backbone.h"
 
-void LFM();
-
+void LFM2Model(int *token_ids, int seq_len, LFM2Config *config, Weights *weights, int batch);
 #endif
