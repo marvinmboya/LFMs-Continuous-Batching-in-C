0665e1ba85efd61cfb622cc1c08ab80ca1dc4f3b
Marvin-desmond <marvindesmond@gmail.com>
2026-01-28 17:21:13 -0500

    load fp32 weights data directly

--- model/utils.c
+++ model/utils.c
@@ -33,68 +33,21 @@ size_t get_file_size_v2(FILE *f){
     return sb.st_size;
 }
 
-uint16_t *read_bin_data(const char *data_path, size_t n){
-    FILE *f = fopen(data_path, "rb");
-    if (!f) PERR("file open error!");
-    size_t fsize = get_file_size_v2(f);
-    size_t read_contents = fsize / sizeof(uint16_t);
-    if (n != read_contents) PERR("read not expected");
-    uint16_t *udata = malloc(n * sizeof(uint16_t));
-    if (!udata) PERR("udata mem error!");
-    fread(udata, sizeof(uint16_t), n, f);
-    fclose(f);
-    return udata;
-}
-
-void read_bin_map_data(const char *weights_path, Weights_Meta *meta, size_t n) {
+void load_map_data(const char *weights_path, Weights_Meta *meta, size_t n) {
     int fd = open(weights_path, O_RDONLY);
     if (fd == -1) PERR("file open error!"); 
     struct stat sb;
     if (fstat(fd, &sb) == -1) PERR("file size error!");
-    size_t fsize = sb.st_size; // no. elements * uint16
-    size_t read_contents = fsize / sizeof(uint16_t);
+    size_t fsize = sb.st_size; // no. elements * float
+    size_t read_contents = fsize / sizeof(float);
     if (n != read_contents) PERR("read not expected");
     void *weights_map = mmap(NULL, fsize, PROT_READ, MAP_PRIVATE, fd, 0);
     if (weights_map == MAP_FAILED) PERR("error mapping file");
-    uint16_t* weights_data = (uint16_t*)weights_map;
-    meta->udata = weights_data;
+    float* weights_data = (float*)weights_map;
+    meta->fdata = weights_data;
     meta->size = fsize;
-    close(fd);
-}
-
-float *convert_bin_float(uint16_t *udata, size_t n){
-    float *fdata = malloc(n * sizeof(float));
-    if (!fdata) PERR("fdata mem error!");
-    #pragma omp parallel for
-    for (int i = 0; i < n; i++){
-        fdata[i] = bf16_to_float32(udata[i]);
-    }
-    free(udata);
-    return fdata;
-}
-
-void convert_bin_map_float(Weights_Meta *meta, size_t n) {
-    float *fdata = mmap(NULL, n * sizeof(float), PROT_READ | PROT_WRITE, 
-                    MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-    if (!fdata) PERR("fdata mem error!");
-    for (int i = 0; i < n; i++){
-        fdata[i] = bf16_to_float32(meta->udata[i]);
-    }
-    meta->fdata = fdata;
-    munmap(meta->udata, meta->size);
     meta->udata = NULL;
-    meta->size = n * sizeof(float);
-}
-
-float* load_data(const char *data_path, size_t n) {
-        uint16_t *udata = read_bin_data(data_path, n);
-        float *fdata = convert_bin_float(udata, n);
-        return fdata;
-}
-
-void load_map_data(const char *weights_path, Weights_Meta *meta, size_t n) {
-    read_bin_map_data(weights_path, meta, n);
-    convert_bin_map_float(meta, n);
+    close(fd);
 }
 
 void debug_print_first_five(float *embed_out, int seq_len, int d_model) {
