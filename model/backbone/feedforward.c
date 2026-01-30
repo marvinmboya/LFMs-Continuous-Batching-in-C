37ffb10347bafee6d1b9f5d3159a55686ef01aa2
Marvin-desmond <marvindesmond@gmail.com>
2026-01-30 10:58:58 -0500

    utilize pre-allocated buffers instead of within-function created buffers

--- model/backbone/feedforward.c
+++ model/backbone/feedforward.c
@@ -1,17 +1,14 @@
 #include "feedforward.h"
 
 void feedforward(
-    float *x_in, float *x_out, const float *w1, const float *v, 
-    const float *w2, int batch, int seq_len, int d_model, int d_hidden
+    float *x_in, float *x_out, const float *w1, const float *v, const float *w2, 
+    Buf *buf, int batch, int seq_len, int d_model, int d_hidden
 ){
-    float *out_w1 = malloc(batch * seq_len * d_hidden * sizeof(float));
-    float *out_v = malloc(batch * seq_len * d_hidden * sizeof(float));
-
-    matmul(x_in, w1, out_w1, batch, seq_len, d_model, d_hidden);
-    matmul(x_in, v, out_v, batch, seq_len, d_model, d_hidden);
-    silu(out_w1, batch * seq_len * d_hidden);
-    elementwise_mul(out_w1, out_v, batch * seq_len * d_hidden);
-    matmul(out_w1, w2, x_out, batch, seq_len, d_hidden, d_model);
+    matmul(x_in, w1, buf->out_w1, batch, seq_len, d_model, d_hidden);
+    matmul(x_in, v, buf->out_v, batch, seq_len, d_model, d_hidden);
+    silu(buf->out_w1, batch * seq_len * d_hidden);
+    elementwise_mul(buf->out_w1, buf->out_v, batch * seq_len * d_hidden);
+    matmul(buf->out_w1, w2, x_out, batch, seq_len, d_hidden, d_model);
 }
 
 void silu(float *x, size_t n) {
