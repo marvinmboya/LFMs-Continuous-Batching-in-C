61da469e793fe0a8889d3962f90b0b0dca968ff2
Marvin-desmond <marvindesmond@gmail.com>
2026-02-04 17:09:58 -0500

    pass cache buf to model func

--- model/lfm.h
+++ model/lfm.h
@@ -4,8 +4,10 @@
 #include "types.h"
 #include "embed.h"
 #include "backbone/backbone.h"
+#include "../cache/init_cache.h"
 
 void LFM2Model(
-    Weights *weights, Buf *buffers, LFM2Config *config, int *token_ids, int seq_len, int batch
+    Weights *weights, Buf *buf, CBuf *cache_buf, 
+    LFM2Config *config, int *token_ids, int seq_len, int batch
 );
 #endif
