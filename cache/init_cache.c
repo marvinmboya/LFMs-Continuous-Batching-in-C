fb795134bc47d5f8cd381ab70fa79741c48d2874
Marvin-desmond <marvindesmond@gmail.com>
2026-02-04 17:54:47 -0500

    fix pointers to create and destroy cache buffers

--- cache/init_cache.c
+++ cache/init_cache.c
@@ -1,32 +1,33 @@
 #include "init_cache.h"
 
 int cache_seq_len = 0;
-static void init_calloc(float **buf, size_t n);
-static void init_malloc(float **buf, size_t n);
+static void init_calloc(float *buf, size_t n);
+static void init_malloc(float *buf, size_t n);
 
 void create_cache_buffers(CBuf *bufs, LFM2Config config, int batch) {
     int NL = 16;
+    bufs->conv_state = malloc(NL * sizeof(float*));
+    bufs->k_cache = malloc(NL * sizeof(float*));
+    bufs->v_cache = malloc(NL * sizeof(float*));
     size_t conv_sz = (size_t)batch * config.d_model * config.k_size;
-    bufs = (CBuf *)malloc(NL * sizeof(CBuf));
     for (int i = 0; i < NL; i++) {
-        init_calloc(&(bufs[i].conv_state), conv_sz);
-        init_malloc(&(bufs[i].k_cache), batch * config.max_seq_len * config.kv_groups * config.head_dim);
-        init_malloc(&(bufs[i].v_cache), batch * config.max_seq_len * config.kv_groups * config.head_dim);
+        init_calloc(bufs->conv_state[i], conv_sz);
+        init_malloc(bufs->k_cache[i], batch * config.max_seq_len * config.kv_groups * config.head_dim);
+        init_malloc(bufs->v_cache[i], batch * config.max_seq_len * config.kv_groups * config.head_dim);
     }
 }
 
-static void destroy_malloc(float *buf) {
-    if (buf) free(buf);
-}
 
-void destroy_cache_buffers(CBuf *bufs) {
+void destroy_cache_buffers(CBuf *buf) {
     int NL = 16;
     for (int i = 0; i < NL; i++) {
-        destroy_malloc(bufs[i].conv_state);
-        destroy_malloc(bufs[i].k_cache);
-        destroy_malloc(bufs[i].v_cache);
+        free(buf->conv_state[i]);
+        free(buf->k_cache[i]);
+        free(buf->v_cache[i]);
     }
-    if (bufs) free(bufs);
+    free(buf->conv_state);
+    free(buf->k_cache);
+    free(buf->v_cache);
 }
 
 void update_cache(CBuf *bufs, LFM2Config *config, const float *k, const float *v, int start, int seq_len, int idx) {
@@ -41,12 +42,12 @@ int get_seq_len() {
     return cache_seq_len;
 }
 
-static void init_calloc(float **buf, size_t n) {
-    *buf = (float *)calloc(n, sizeof(float));
-    if (!(*buf)) PERR("calloc error!");
+static void init_calloc(float *buf, size_t n) {
+    buf = (float *)calloc(n, sizeof(float));
+    if (!buf) PERR("calloc error!");
 }
 
-static void init_malloc(float **buf, size_t n) {
-    *buf = malloc(n * sizeof(float));
-    if (!(*buf)) PERR("malloc error!");
+static void init_malloc(float *buf, size_t n) {
+    buf = malloc(n * sizeof(float));
+    if (!buf) PERR("malloc error!");
 }
