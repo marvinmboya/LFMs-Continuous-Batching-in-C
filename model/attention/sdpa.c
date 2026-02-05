89d5056f717091b48f9f86391fd85b5dbe446e8a
Marvin-desmond <marvindesmond@gmail.com>
2026-02-05 16:21:36 -0500

    attn computation for different q, k seq lens

--- model/attention/sdpa.c
+++ model/attention/sdpa.c
@@ -1,9 +1,10 @@
 #include "sdpa.h"
 
 void sdpattention(
-    Buf *buf, int batch, int seq_len, int heads, int head_dim
+    Buf *buf, int batch, int seq_len, int decode_start, int heads, int head_dim
 ){ 
-    matmul_implicit_transpose(buf->q_t, buf->k_expand, buf->scores, batch, heads, seq_len, head_dim);
-    softmax_last(buf->scores, buf->norm_scores, batch, heads, seq_len, seq_len);
-    matmul_higher(buf->norm_scores, buf->v_expand, buf->attn_out, batch, heads, seq_len, head_dim);
+    int q_seq_len = seq_len, k_seq_len = decode_start + seq_len;
+    matmul_implicit_transpose(buf->q_t, buf->k_expand, buf->scores, batch, heads, q_seq_len, k_seq_len, head_dim);
+    softmax_last(buf->scores, buf->norm_scores, batch, heads, q_seq_len, k_seq_len);
+    matmul_higher(buf->norm_scores, buf->v_expand, buf->attn_out, batch, heads, q_seq_len, k_seq_len, head_dim);
 }
