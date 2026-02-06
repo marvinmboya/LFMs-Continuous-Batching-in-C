fa5d97d3d909d11a3eba78697aa1f06f7f142236
Marvin-desmond <marvindesmond@gmail.com>
2026-02-05 22:48:53 -0500

    implement functions to mutate prefill and decode in cached gsc

--- model/gsc.c
+++ model/gsc.c
@@ -1,4 +1,5 @@
 #include "gsc.h"
+#include <stdio.h>
 
 void gscblock(
     float *x_in, GSCWeights *gsc_w, Buf *buf, CBuf *cache_buf, int batch, 
@@ -12,12 +13,95 @@ void gscblock(
     memcpy(buf->x, buf->BCx_t + (sz * 2), fsz);
     elementwise_mul(buf->B, buf->x, sz);
     float *Bx = buf->B;
-    int co_sz = get_conv_out_size(seq_len, k_size, k_size - 1, 1);
-    depthwise_conv1d(Bx, gsc_w->conv, buf->conv_out, d_model, seq_len, co_sz, k_size, k_size - 1);
-    slice_conv_out(buf->conv_out, buf->conv_out_sliced, d_model, co_sz, seq_len);
+    if (decode_start > 0){
+        size_t sz = batch * d_model * k_size, 
+          fsz = sz * sizeof(float);
+        float *conv_state = malloc(fsz);
+        gsc_mutate_decode(conv_state, cache_buf, Bx, l_idx, batch, seq_len, d_model, k_size);
+        conv_decode_sum(conv_state, gsc_w->conv,
+        buf->conv_out_sliced, batch, d_model, k_size);
+        free(conv_state);
+    } else {
+        gsc_mutate_prefill(cache_buf, Bx, l_idx, batch, seq_len, d_model, k_size);
+        get_sliced_Bx(buf, gsc_w, Bx, d_model, seq_len, k_size);
+    }
     elementwise_mul(buf->conv_out_sliced, buf->C, batch * d_model * seq_len);
     transpose_last(batch, d_model, seq_len, buf->conv_out_sliced, buf->conv_out_t);
     matmul(buf->conv_out_t, gsc_w->w2, buf->x_out, batch, seq_len, d_model, d_model);
+}
+
+void gsc_mutate_prefill(
+    CBuf *cache_buf, const float *Bx, int l_idx, 
+    int batch, int seq_len, int d_model, int k_size
+) {
+    #pragma omp parallel for
+    for (int i = 0; i < d_model; i ++){
+        memcpy(
+            cache_buf->conv_state[l_idx] + (i * k_size), 
+            Bx + ((i + 1) * seq_len - k_size),
+            k_size * sizeof(float)
+        );
+    }
+}
+
+void gsc_mutate_decode(
+    float *conv_state, CBuf *cache_buf, const float *Bx, int l_idx, 
+    int batch, int seq_len, int d_model, int k_size
+) {
+    int cache_id = k_size - 1;
+    size_t sz = batch * d_model * k_size, 
+          fsz = sz * sizeof(float);
+    memcpy(conv_state, cache_buf->conv_state[l_idx], fsz);
+    roll(conv_state, d_model);
+    #pragma omp simd
+    for (int d = 0; d < d_model; ++d) {
+        conv_state[d * k_size + cache_id] = Bx[d];
+    }
+    memcpy(cache_buf->conv_state[l_idx], conv_state, fsz);
+}
+
+void roll(float *x, int d_model){
+    #pragma omp parallel for
+    for (int j = 0; j < d_model; j++) {
+        int base = j * 3;
+        float tmp = x[base];
+        x[base] = x[base + 1];
+        x[base + 1] = x[base + 2];
+        x[base + 2] = tmp;
+    }
+}
+
+void conv_decode_sum(
+    const float* conv_state, const float* weight,
+    float* conv_out, int batch, int d_model, int k_size
+) {
+    #pragma omp parallel for
+    for (int b = 0; b < batch; ++b) {
+        for (int d = 0; d < d_model; ++d) {
+            float sum = 0.0f;
+            #pragma omp simd reduction(+:sum)
+            for (int k = 0; k < k_size; ++k) {
+                sum += conv_state[
+                    b * d_model * k_size 
+                    + d * k_size + k] 
+                * weight[d * k_size + k];
+            }
+            conv_out[b * d_model + d] = sum;
+        }
+    }
+}
+
+
+void get_sliced_Bx(
+    Buf *buf, GSCWeights *gsc_w, float *Bx, 
+    int d_model, int seq_len, int k_size
+) {
+    int co_sz = get_conv_out_size(seq_len, k_size, k_size - 1, 1);
+    depthwise_conv1d(
+        Bx, gsc_w->conv, buf->conv_out, d_model, 
+        seq_len, co_sz, k_size, k_size - 1
+    );
+    slice_conv_out(buf->conv_out, buf->conv_out_sliced, d_model, co_sz, seq_len);
 }
 
 int get_conv_out_size(int in_size, int k_size, int p_size, int stride){
